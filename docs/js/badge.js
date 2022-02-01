9426a4f75edf1332b7d1743f32581798da0f8e3f
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2022-02-01 11:53:47 +0530

    Added badge doc page

--- docs/js/badge.js
+++ docs/js/badge.js
@@ -1,16 +1,9 @@
 /**Badge JS**/
 
 const badgeBtn = document.querySelector('#badgeBtn')
-const hideBadge = document.querySelector('#hideBadge')
 
 badgeBtn.addEventListener('click', checkBadge)
 
 function checkBadge() {
-  if (hideBadge.classList.contains('badge-content')) {
-    hideBadge.classList.remove('badge-content', 'position-absolute', 'sm-badge')
-    hideBadge.classList.add('hidden')
-  } else {
-    hideBadge.classList.remove('hidden')
-    hideBadge.classList.add('badge-content', 'position-absolute', 'sm-badge')
-  }
+  badgeBtn.childNodes[1].classList.toggle('hidden')
 }
